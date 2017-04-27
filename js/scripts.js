c74a5f8e35d1b85f3b280a4bdac13c43268c38cd
Calvin Cushman and Karina Roush <calvin@>
2017-04-27 11:03:31 -0700

    add hide method to button

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(function() {
-  var animal = ('#animal-select')
-    
-})
+  $(".btn").click(function(){
+    $(".info").hide();
+
+  var animal = $("#animals").val()
+
+  if(animal === "Turkey") {
+    $(".turkey-info").show()
+  }
+  else if(animal === "Frogs") {
+    $(".frog-info").show()
+  }
+  else {
+    $(".bacteria-info").show()
+  };
+
+});
+});
